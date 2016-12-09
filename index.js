daed0857934e89eff7abae64ee23909cca5002c5
杨涛 <spryti@qq.com>
2016-12-09 10:20:28 +0800

    优化注释

--- index.js
+++ index.js
@@ -4,19 +4,17 @@ module.exports = function (content, file, options) {
 
     var this_id = file.id.replace(file.ext, '');
 
-    content = content.replace(/["']!([^'"\s]+)["']/g, function (str) {
-        return str.replace('.js', '').replace('!this', this_id);
-    });
-
-    return content.replace(/<script.*?data-name=["'](.*?)["'].*?>([\s\S]*?)<\/script>/g, function (str, name, value) {
-        name = file.realpathNoExt + name.replace('this', '');
-
-        var f = fis.file.wrap(name);
-        f.setContent(value);
-        fis.compile(f);
-        file.derived.push(f);
-        return '';
-    });
+    return '<!DOCTYPE html>\r\n' + content
+            .replace(/["']![^'"\s]+["']/g, function (str) {
+                return str.replace('.js', '').replace('!this', this_id);
+            })
+            .replace(/<script.*?data-name=["'](.*?)["'].*?>([\s\S]*?)<\/script>/g, function (str, name, value) {
+                name = file.realpathNoExt + name.replace('this', '');
+                var f = fis.file.wrap(name);
+                f.setContent(value);
+                fis.compile(f);
+                file.derived.push(f);
+                return '';
+            })
+            .match(/<html.*?>[\s\S]*?<\/html>/);
 };
-
-
