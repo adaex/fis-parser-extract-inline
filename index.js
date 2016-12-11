ece3271e3c5b90c1f4f63374b025d304dd77affe
杨涛 <spryti@qq.com>
2016-12-11 12:52:04 +0800

    优化data-loader

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 
 module.exports = function (content, file, options) {
 
-    var this_id = file.id.replace(file.ext, '');
-    var libs = '';
+    var this_id = '', libs = '';
+
+    this_id = file.getId().replace(file.ext, '');
+
     options.libs.forEach(function (lib) {
-        libs += '<script type="text/javascript" src="' + lib + '"></script>\r\n'
+        libs += '<script type="text/javascript" src="' + lib + '" data-loader></script>\r\n'
     });
 
     return '<!DOCTYPE html>\r\n' + content
@@ -15,9 +17,14 @@ module.exports = function (content, file, options) {
             .replace(/<(script|template).*?data-name=["'](.*?)["'].*?>([\s\S]*?)<\/(script|template)>/g, function (str, type, name, value) {
                 name = file.realpathNoExt + name.replace('this', '');
                 var f = fis.file.wrap(name);
+                f.cache = file.cache;
                 f.setContent(value);
-                fis.compile(f);
+                fis.compile.process(f);
+                f.links.forEach(function (derived) {
+                    file.addLink(derived);
+                });
                 file.derived.push(f);
+                file.addRequire(f.getId());
                 return '';
             })
             .replace(/<script.*?>[\s\S]*?<\/script>/, function (str) {
